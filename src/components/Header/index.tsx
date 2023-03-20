056a43f063e18a20bc4b07f5f3ec1b61db786ba4
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-20 11:39:10 -0400

    styling home page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,28 @@ import style from './Header.module.scss'
 export function Header() {
     return (
         <div className={style.headerContent}>
-            Header
+            <a className={style.logoLink} href='https://www.hakacrossfit.com.br'>
+                <img className={style.logo} src="./images/full-logos/full-white-logo.png" alt="" />
+            </a>
+
+            <nav className={style.links}>
+                <a href=''>Início</a>
+                <a href=''>Padrão Haka</a>
+                <a href=''>Horários</a>
+                <a href=''>Planos</a>
+                <a href=''>Galeria</a>
+                <a href=''>Contato</a>
+            </nav>
+
+            <nav className={style.langs}>
+                <button>
+                    <img src='./images/flags/br-flag.svg' alt="" />
+                </button>
+
+                <button>
+                    <img src='./images/flags/uk-flag.svg' alt="" />
+                </button>
+            </nav>
         </div>
     )
 }
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -4,12 +4,16 @@ export function Title() {
     return (
         <main className={style.main}>
             <div className={style.titleImage}>
-                <img className={style.logo} src="/images/logos/logo.svg" alt="" />
+                {/*<img className={style.fire} src="/images/fire.jpg" alt="" />*/}
+                {/*<img className={style.logo} src="/images/logos/logo.svg" alt="logo" />*/}
             </div>
 
             <section className={style.headTitles}>
                 <span className={style.title1}>Transforme seu corpo.</span>
                 <span className={style.title2}>Mude sua vida.</span>
+                <button type='button'>
+                    Comece hoje
+                </button>
             </section>
         </main>
     )
