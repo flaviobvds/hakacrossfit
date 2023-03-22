697f30ea5dc5c20269475006ae2f6c1502b4e160
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-22 00:57:42 -0400

    improved Feature Cards design

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -7,15 +7,15 @@ export function Features() {
 
             <section className={styles.featureCardsSection}>
                 <div className={styles.featureCard}>
-                    <img 
-                        src="./images/icons/health.png" 
-                        alt="health" 
-                        height="60"
+                    <img
+                        src="./images/icons/health.png"
+                        alt="health"
+                        height="70"
                         className={styles.healthIcon}
                     />
-                    <h1 className={styles.cardTitle}>
-                        SAÚDE EM PRIMEIRO LUGAR
-                    </h1>
+                    <header className={styles.cardTitle}>
+                        <span>Saúde em Primeiro Lugar</span>
+                    </header>
                     <p className={styles.cardText}>
                         Priorizamos sua <span>saúde</span> antes de qualquer outro resultado.
                         Nossos treinos são <span>adaptados</span> para você e seu corpo.
@@ -23,32 +23,33 @@ export function Features() {
                 </div>
 
                 <div className={styles.featureCard}>
-                    <img 
-                        src="./images/icons/health.png" 
-                        alt="health" 
-                        height="60"
+                    <img
+                        src="./images/icons/coach-1.png"
+                        alt="coach"
+                        height="70"
                         className={styles.healthIcon}
                     />
-                    <h1 className={styles.cardTitle}>
-                        SAÚDE EM PRIMEIRO LUGAR
-                    </h1>
+                    <header className={styles.cardTitle}>
+                        <span>Acompanhamento Profissional</span>
+                    </header>
                     <p className={styles.cardText}>
-                        Priorizamos sua <span>saúde</span> antes de qualquer outro resultado.
-                        Nossos treinos são <span>adaptados</span> para você e seu corpo.
+                        Conte com o acompanhamento personalizado de profissionais capacitados e <span>credenciados </span> 
+                        que buscarão o seu <span>melhor desempenho</span>.
+
                     </p>
                 </div>
 
 
                 <div className={styles.featureCard}>
-                    <img 
-                        src="./images/icons/health.png" 
-                        alt="health" 
+                    <img
+                        src="./images/icons/health.png"
+                        alt="health"
                         height="60"
                         className={styles.healthIcon}
                     />
-                    <h1 className={styles.cardTitle}>
-                        SAÚDE EM PRIMEIRO LUGAR
-                    </h1>
+                    <header className={styles.cardTitle}>
+                        <span>SAÚDE EM PRIMEIRO LUGAR</span>
+                    </header>
                     <p className={styles.cardText}>
                         Priorizamos sua <span>saúde</span> antes de qualquer outro resultado.
                         Nossos treinos são <span>adaptados</span> para você e seu corpo.
