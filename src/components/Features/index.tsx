4c86cc155815459a87cb7b7878f7f2ad10be499d
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-22 03:20:59 -0400

    improved Features component

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -5,11 +5,24 @@ import { FeatureCards } from '../FeatureCards'
 export function Features() {
     return (
         <div className={styles.featuresContent}>
-            <FeatureCards/>
+            <FeatureCards />
             <h1 className={styles.featuresTitle}>
-                <span>SIM,</span> <br/>
-                O Crossfit é para você
+                <header className={styles.bgHeader}>
+                    <span>SIM</span>
+                </header>
+                Crossfit é para você
             </h1>
+
+            <div className={styles.textContainer}>
+                Nossa metodologia não escolhe público. Funciona com todos.
+                Não trata-se de mágica, mas sim de <span>resultados</span>.
+                <br/>
+                Através do Crossfit, conseguimos entregar evolução mais
+                rápida e de forma mais segura.
+                Conheça a revolução do mundo fitness que já mudou <span>milhões </span>
+                de vidas por todo o mundo.
+
+            </div>
         </div>
     )
 }
