a7c358e05011e237501500e16e49aac34d276cb0
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-03-27 15:12:25 -0400

    created murph component

--- src/components/Murph/index.tsx
+++ src/components/Murph/index.tsx
@@ -9,31 +9,27 @@ export function Murph() {
                 </span>
             </header>
 
-            <div className={styles.affiliateContent}>
-                <div className={styles.affiliateImage}>
-                    <img
-                        src="./images/coach.jpg"
-                        alt="crossfit_certificate"
-                        className={styles.coachImage}
-                    />
-                    <img
-                        src="./images/certificate.png"
-                        alt="crossfit_certificate"
-                        className={styles.crossfitCertificate}
-                    />
+            <div className={styles.murphContent}>
+                <div className={styles.murphList}>
+                    <ul>
+                        <li>1 Mile Run</li>
+                        <li>100 Pull-Ups</li>
+                        <li>200 Push-Ups</li>
+                        <li>300 Squats</li>
+                        <li>1 Mile Run</li>
+                    </ul>
+                    <span>"MURPH"</span>
                 </div>
 
-                <div className={styles.affiliateText}>
+                <div className={styles.murphText}>
                     <span>Coloque seus limites à prova</span>
-                    <p className={styles.test}>
-                        Crossfit pode ser praticado em qualquer lugar.
-                        Nas ruas, nos parques, em casa. No entanto, a
-                        expertise de um coach <span>CrossFit® </span>
-                        credenciado faz toda a diferença. Somos um box
-                        credenciado oficial da marca CrossFit®, o que garante
-                        a você a certeza de que a metodologia do seu treino
-                        de Crossfit está sendo aplicada da forma certa
-                        e segura por um profissional habilitado.
+                    <p className={styles.murphPar}>
+                        Uma vez por ano temos um treino que vai testar seus
+                        limites. O <span>Murph</span> é um WOD longo que irá desafiar você
+                        e serve como <span>benchmark</span> no mundo do Crossfit.
+                        No Haka tratamos o Murph como um evento anual para 
+                        o qual você irá se preparar e ao final terá a satisfação
+                        de ter superado este imenso desafio.
                     </p>
                 </div>
             </div>
