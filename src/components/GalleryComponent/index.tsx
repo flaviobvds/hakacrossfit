34649be4535212b2aed76148f3513cfe699480e9
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-04-26 23:44:58 -0400

    working on GalleryComponent

---
+++ src/components/GalleryComponent/index.tsx
@@ -0,0 +1,9 @@
+import styles from './GalleryComponent.module.scss'
+
+export function GalleryComponent() {
+    return (
+        <div className={styles.gallery}>
+            
+        </div>
+    )
+}
