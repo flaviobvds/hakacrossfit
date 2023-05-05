056798879216528c458795bb426dad94cca73aea
flaviobvds <40229603+flaviobvds@users.noreply.github.com>
2023-05-05 03:00:59 -0400

    styling Footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,6 @@
 import styles from './Footer.module.scss'
+import { FaWhatsapp, FaInstagram, } from 'react-icons/fa'
+import { MdOutlineEmail } from 'react-icons/md';
 
 export function Footer() {
     return (
@@ -14,11 +16,48 @@ export function Footer() {
             </div>
 
             <div className={styles.times}>
+                <span>Horários</span>
+                <div className={styles.timesContainer}>
+                    <div className={styles.days}>
+                        Seg: <br />
+                        Ter: <br />
+                        Qua: <br />
+                        Qui: <br />
+                        Sex: <br />
+                        Sáb: <br />
+                        Dom:
+                    </div>
+
+                    <div className={styles.dayTime}>
+                        06:00 - 09:00 | 16:00 - 22:00<br />
+                        06:00 - 09:00 | 16:00 - 22:00<br />
+                        06:00 - 09:00 | 16:00 - 22:00<br />
+                        06:00 - 09:00 | 16:00 - 22:00<br />
+                        06:00 - 09:00 | 16:00 - 22:00<br />
+                        09:00 - 12:00 <br />
+                        -
+                    </div>
+                </div>
 
             </div>
 
-            <div className={styles.socials}>
+            <div className={styles.contact}>
+                Contato
 
+                <div className={styles.socials}>
+                    <div className={styles.social}>
+                        <FaWhatsapp className={styles.icon} size={20} />
+                        (95) 99124-5752
+                    </div>
+                    <div className={styles.social}>
+                        <FaInstagram className={styles.icon} size={20} />
+                        @hakacrossfit
+                    </div>
+                    <div className={styles.social}>
+                        <MdOutlineEmail className={styles.icon} size={20} />
+                        hakacrossfit@gmail.com
+                    </div>
+                </div>
             </div>
         </div>
     )
